9c96db6d1a2827b630e722a62a44851a80dc0c6e
KazancevaYuliya <87359439+KazancevaYuliya@users.noreply.github.com>
2022-01-19 22:01:11 +0300

    Add files via upload

---
+++ ДЗ/script.js
@@ -0,0 +1,33 @@
+// ---1.---
+//const inputElement = document.querySelector('#input')
+//const outputElement = document.querySelector('#output')
+
+//function handleInput(event) {
+//console.log('clik')
+//const { value } = inputElement
+//outputElement.textContent = value
+//}
+
+//inputElement.addEventListener('input', handleInput)
+
+
+
+// ---2.---
+
+//const formElement = document.querySelector('#formId')
+//const formInputElement = document.querySelector('#formInput')
+//const listElement = document.querySelector('#list1')
+
+//function handleSubmitForm(event) {
+    //event.preventDefault()
+
+    //const { value } = formInputElement
+    //const listItemElement = document.createElement('li')
+
+    //listItemElement.textContent = value
+    //listElement.append(listItemElement)
+
+    //formElement.reset()
+//}
+
+//formElement.addEventListener('submit', handleSubmitForm)
